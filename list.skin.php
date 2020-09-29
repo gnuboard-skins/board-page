1d4f3aa4bc40cf2a62829f4127351ed1cbc73268
박경종 <kevinpark@webace.co.kr>
2020-09-29 16:53:40 +0900

    게시판 목록 코드정리 및 스타일 코드 정리

--- list.skin.php
+++ list.skin.php
@@ -1,17 +1,19 @@
 <?php
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
-// ------------------------------------------------------------------
-// css/js 추가
-add_stylesheet('<link rel="stylesheet" href="'.$board_skin_url.'/style.css">', 0);
-add_javascript("<script src='{$board_skin_url}/script.js'></script>", 1);
+/**
+ * css/js 추가
+ */
 add_javascript('<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js" integrity="sha512-uto9mlQzrs59VwILcLiRYeLKPPbS/bT71da/OEBYEwcdNUk8jYIy+D176RYoop1Da+f9mvkYrmj5MCLZWEtQuA==" crossorigin="anonymous"></script>', 0);
 add_javascript('<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-visible/1.2.0/jquery.visible.min.js" integrity="sha512-771ZvVCYr4EfUGXr63AcX7thw7EKa6QE1fhxi8JG7mPacB/arC0cyvYPXKUkCrX2sYKnnFCZby3ZZik42jOuSQ==" crossorigin="anonymous"></script>', 1);
+add_javascript("<script src='{$board_skin_url}/script.js'></script>", 2);
 add_stylesheet('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" integrity="sha512-aOG0c6nPNzGk+5zjwyJaoRUgCdOrfSDhmMID2u4+OIslr0GjpLKo7Xm0Ao3xmpM4T8AmIouRkqwj1nrdVsLKEQ==" crossorigin="anonymous" />', 0);
+add_stylesheet('<link rel="stylesheet" href="'.$board_skin_url.'/style.css">', 1);
 
-// ------------------------------------------------------------------
-// 분류 설정
-// 분류를 사용하는데 분류가 없을 경우 첫번째 분류로 이동 (전체 없음)
+/**
+ * 분류 설정
+ * 분류를 사용하는데 분류가 없을 경우 첫번째 분류로 이동 (전체 없음)
+ */
 if($board['bo_use_category']) {
 
     $ca_list = explode('|',$board['bo_category_list']);
@@ -32,12 +34,14 @@ if($board['bo_use_category']) {
     }
 }
 
-// ------------------------------------------------------------------
-// 마지막으로 작성한 글
+/**
+ * 마지막으로 작성한 글
+ */
 $v = $list[0];
 
-// ------------------------------------------------------------------
-// 최신글 기능 {{latest, bo_table, skin, rows, subject_len}}
+/**
+ * 최신글 기능 {{latest, bo_table, skin, rows, subject_len}}
+ */
 preg_match_all('/{{([a-z,_0-9]+)}}/', $v['wr_content'], $matchs);
 foreach($matchs[1] as $idx=>$match) {
     list($func, $board_table, $skin, $rows, $subject_len) = explode(',',$match);
@@ -47,8 +51,9 @@ foreach($matchs[1] as $idx=>$match) {
     }
 }
 
-// ------------------------------------------------------------------
-// 분류 출력
+/**
+ * 분류 출력
+ */
 if(isset($category_list)) {
     $li_list = '';
     foreach($category_list as $ca) {
@@ -56,28 +61,26 @@ if(isset($category_list)) {
     }
     echo "<nav class='page-category scroll-fixed full-width' data-offset-top='0'><div class='wrap'><ul>{$li_list}</ul></div></nav>";
 }
-?>
 
-<?php if ($write_href) { ?>
-    <div class="page-contents-buttons right">
-        <a href="<?php echo $write_href?>" class="btn">
-            <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
-            페이지 수정
-        </a>
+/**
+ * 페이지 수정 버튼
+ */
+$write_btn = '';
+if ($write_href) {
+    $write_btn = <<<TAG
+    <div class='page-contents-buttons right'>
+        <a href="{$write_href}&sca={$sca}" class="btn"> <i class="fa fa-pencil-square-o" aria-hidden="true"></i> 페이지 수정 </a>
     </div>
-<?php } ?>
+TAG;
+}
 
-<?php echo $board['bo_content_head']?>
-<div id="page-contents" class="page-contents">
-    <?php echo $v['wr_content']?>
-</div>
-<?php echo $board['bo_content_tail']?>
-
-<?php if ($write_href) { ?>
-    <div class="page-contents-buttons">
-        <a href="<?php echo $write_href?>" class="btn">
-            <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
-            페이지 수정
-        </a>
-    </div>
-<?php } ?>
+/**
+ * 컨텐츠 출력
+ */
+echo $write_btn;
+echo $board['bo_content_head'];
+echo <<<CONTENTS
+<div id="page-contents" class="page-contents">{$v['wr_content']}</div>
+CONTENTS;
+echo $board['bo_content_tail'];
+echo $write_btn;
