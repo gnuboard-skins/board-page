f3c05135d20f40ac6c8bf5fb49b5cc574e108827
박경종 <kevinpark@webace.co.kr>
2020-09-29 23:25:50 +0900

    뷰 페이지 스타일 정리

--- write_update.skin.php
+++ write_update.skin.php
@@ -1,6 +1,8 @@
-<?php
-if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+<?php if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
+/**
+ * 게시글 작성 후 뷰 페이지로 이동하지 않도록 함
+ */
 if($board['bo_use_category']) {
     goto_url(G5_HTTP_BBS_URL.'/board.php?bo_table='.$bo_table."&sca={$_POST['ca_name']}");
 } else {
