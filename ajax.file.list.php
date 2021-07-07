e6db192fe0acfe4225f8a8b245223baf4a9ae0cd
박경종 <kevinpark@webace.co.kr>
2021-07-08 00:34:10 +0900

    Attached Cloud 적용

--- ajax.file.list.php
+++ ajax.file.list.php
@@ -1,31 +1,38 @@
 <?php
 include_once('../../../../../common.php');
 
-$wr_id = -1;
-if($sca) {
-    foreach(explode('|',$board['bo_category_list']) as $idx=>$ca) {
-        if($sca==$ca) $wr_id = $idx;
-    }
+if(!$member['mb_no']) {
+    header("Content-Type: application/json");
+    echo json_encode([
+        'success'=>false,
+        'bf_no'=>0,
+        'image'=>false,
+        'path'=> '',
+        'msg'=> '파일업로드를 위해서는 로그인이 필요합니다.'
+    ]);
+    exit;
 }
 
 $table = $g5['board_file_table'];
 
 $result = sql_query("
- select bf_source as name, bf_file, bf_no, bf_filesize as size, bf_width as width, bf_height as height
- from {$table} where `bo_table` = '{$bo_table}' and `wr_id` = {$wr_id}
+ select bf_source as name, bf_no, bf_filesize as size, bf_width as width, bf_height as height, bf_thumburl as thumb, bf_type
+ from {$table} where
+                  `bo_table` = '{$bo_table}' and
+                  (
+                  `wr_id` = {$wr_id} or
+                  (`wr_id` = -1 and `bf_download` = {$member['mb_no']})
+                  )
 ");
 
 $list = [];
 while ($row = sql_fetch_array($result)) {
-    $row['path'] = '/data/file/'.$bo_table.'/'.$row['bf_file'];
+    $row['path'] = $row['thumb'];
     $row['image'] = true;
-    if(is_file(G5_DATA_PATH.'/file/'.$bo_table.'/'.$row['bf_file'])) {
-        $row['mime'] = mime_content_type(G5_DATA_PATH.'/file/'.$bo_table.'/'.$row['bf_file']);
-        if(strpos($row['mime'],'image') === false) {
-            $ext = pathinfo($row['name'],PATHINFO_EXTENSION);
-            $row['thumb'] = $board_skin_url.'/img/extensions/'.$ext.'.svg';
-            $row['image'] = false;
-        }
+    if($row['bf_type']==0) {
+        $row['image'] = false;
+        $ext = pathinfo($row['name'],PATHINFO_EXTENSION);
+        $row['thumb'] = $board_skin_url.'/img/extensions/'.$ext.'.svg';
     }
     $list[] = $row;
 }
--- ajax.file.upload.php
+++ ajax.file.upload.php
@@ -1,125 +1,73 @@
-<?php
-include_once('../../../../../common.php');
+<?php include_once('../../../../../common.php');
 
-// 디렉토리가 생성
-@mkdir(G5_DATA_PATH.'/file/'.$bo_table, G5_DIR_PERMISSION);
-@chmod(G5_DATA_PATH.'/file/'.$bo_table, G5_DIR_PERMISSION);
-
-$wr_id = -1;
-if($sca) {
-    foreach(explode('|',$board['bo_category_list']) as $idx=>$ca) {
-        if($sca==$ca) $wr_id = $idx;
-    }
-}
-
-$chars_array = array_merge(range(0,9), range('a','z'), range('A','Z'));
-
-$upload = [
-    'file'  => '',
-    'source'=> '',
-    'filesize'=>'',
-    'image' => [
-        0=>0,
-        1=>0,
-        2=>0
-    ],
-    'fileurl'=>'',
-    'thumburl'=>'',
-    'storage'=>'',
-    'del_check'=>false
+$response = [
+    'success'=>false,
+    'bf_no'=>0,
+    'image'=>false,
+    'path'=> '',
+    'msg'=>'파일업로드를 위해서는 로그인이 필요합니다.'
 ];
-$tmp_file  = $_FILES['file']['tmp_name'];
-$filesize  = $_FILES['file']['size'];
-$filename  = $_FILES['file']['name'];
-$filename  = get_safe_filename($filename);
-$dest_file = null;
 
-$success = false;
-$file_path = '';
-$bf_no = 0;
-$is_image = false;
+if(!$member['mb_no']) {
+    header("Content-Type: application/json");
+    echo json_encode($response);
+    exit;
+}
+$response['msg'] = '';
 
-if (is_uploaded_file($tmp_file)) {
+try {
+    $ac = new AttachedCloud();
+    $file = $ac->upload();
+    if($file['name']) {
+        $bf_download = 0;
+        if(!$wr_id) {
+            $wr_id = -1;
+            $bf_download = $member['mb_no'];
+        }
 
-    //=================================================================\
-    // 090714
-    // 이미지나 플래시 파일에 악성코드를 심어 업로드 하는 경우를 방지
-    // 에러메세지는 출력하지 않는다.
-    //-----------------------------------------------------------------
-    $timg = @getimagesize($tmp_file);
-    // image type
-    if (
-        preg_match("/\.({$config['cf_image_extension']})$/i", $filename) ||
-        preg_match("/\.({$config['cf_flash_extension']})$/i", $filename)
-    ) {
-        if ($timg['2'] < 1 || $timg['2'] > 16) return;
-    }
-    //=================================================================
+        if($file['image-size'][2]==0) {
+            $ext = pathinfo($file['name'],PATHINFO_EXTENSION);
+            $file['thumb'] = $board_skin_url.'/img/extensions/'.$ext.'.svg';
+        }
 
-    $upload['image'] = $timg;
-
-    // 프로그램 원래 파일명
-    $upload['source'] = $filename;
-    $upload['filesize'] = $filesize;
-
-    // 아래의 문자열이 들어간 파일은 -x 를 붙여서 웹경로를 알더라도 실행을 하지 못하도록 함
-    $filename = preg_replace("/\.(php|pht|phtm|htm|cgi|pl|exe|jsp|asp|inc)/i", "$0-x", $filename);
-
-    shuffle($chars_array);
-    $shuffle = implode('', $chars_array);
-
-    // 첨부파일 첨부시 첨부파일명에 공백이 포함되어 있으면 일부 PC에서 보이지 않거나 다운로드 되지 않는 현상이 있습니다. (길상여의 님 090925)
-    $upload['file'] = abs(ip2long($_SERVER['REMOTE_ADDR'])).'_'.substr($shuffle,0,8).'_'.replace_filename($filename);
-
-    $dest_file = G5_DATA_PATH.'/file/'.$bo_table.'/'.$upload['file'];
-
-    // 업로드가 안된다면 에러메세지 출력하고 죽어버립니다.
-    $error_code = move_uploaded_file($tmp_file, $dest_file) or die($_FILES['bf_file']['error']);
-
-    // 올라간 파일의 퍼미션을 변경합니다.
-    chmod($dest_file, G5_FILE_PERMISSION);
-
-    if (!get_magic_quotes_gpc()) {
-        $upload['source'] = addslashes($upload['source']);
-    }
-
-    $row = sql_fetch("
+        $row = sql_fetch("
  select max(bf_no) as max_bf_no
  from {$g5['board_file_table']}
  where bo_table = '{$bo_table}' and wr_id = {$wr_id}
 ");
-    $bf_no = (int)$row['max_bf_no']+1;
+        $bf_no = (int)$row['max_bf_no']+1;
 
-    $sql = " insert into {$g5['board_file_table']}
+        $sql = " insert into {$g5['board_file_table']}
             set bo_table = '{$bo_table}',
                  wr_id = {$wr_id},
                  bf_no = '{$bf_no}',
-                 bf_source = '{$upload['source']}',
-                 bf_file = '{$upload['file']}',
+                 bf_source = '{$file['name']}',
+                 bf_file = '{$file['name']}',
                  bf_content = '',
-                 bf_fileurl = '{$upload['fileurl']}',
-                 bf_thumburl = '{$upload['thumburl']}',
-                 bf_storage = '{$upload['storage']}',
-                 bf_download = 0,
-                 bf_filesize = '".(int)$upload['filesize']."',
-                 bf_width = '".(int)$upload['image'][0]."',
-                 bf_height = '".(int)$upload['image'][1]."',
-                 bf_type = '".(int)$upload['image'][2]."',
+                 bf_fileurl = '{$file['src']}',
+                 bf_thumburl = '{$file['thumb']}',
+                 bf_storage = '',
+                 bf_download = '{$bf_download}',
+                 bf_filesize = '".(int)$file['file-size']."',
+                 bf_width = '".(int)$file['image-size'][0]."',
+                 bf_height = '".(int)$file['image-size'][1]."',
+                 bf_type = '".(int)$file['image-size'][2]."',
                  bf_datetime = '".G5_TIME_YMDHIS."' ";
-    sql_query($sql);
-    $success = true;
-    $file_path = '/data/file/'.$bo_table.'/'.$upload['file'];
-    $mime = mime_content_type(G5_DATA_PATH.'/file/'.$bo_table.'/'.$upload['file']);
-    if(strpos($mime,'image') !== false) {
-        $is_image = true;
+        sql_query($sql);
+        $response['success'] = true;
+        $response['bf_no'] = $bf_no;
+        $response['path'] = $file['src'];
+        $response['thumb'] = $file['thumb'];
+        $response['width'] = $file['image-size'][0];
+        $response['height'] = $file['image-size'][1];
+        $response['size'] = $file['file-size'];
+        if(strpos($file['contentType'],'image') !== false) {
+            $response['image'] = true;
+        }
     }
+} catch (Exception $e) {
+    $response['msg'] = $e->getMessage();
 }
 
-
 header("Content-Type: application/json");
-echo json_encode([
-    'success'=>$success,
-    'bf_no'=>$bf_no,
-    'image'=>$is_image,
-    'path'=> $file_path
-]);
+echo json_encode($response);
